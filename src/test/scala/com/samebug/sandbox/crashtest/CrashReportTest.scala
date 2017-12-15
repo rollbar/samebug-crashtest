3e09478bd7603dc67e51260521b439fccd445625
Poroszkai Daniel <daniel.poroszkai@samebug.io>
2017-12-15 11:39:36 +0100

    sentry (I do not see the report on the web dashboard)

--- src/test/scala/com/samebug/sandbox/crashtest/CrashReportTest.scala
+++ src/test/scala/com/samebug/sandbox/crashtest/CrashReportTest.scala
@@ -1,16 +1,31 @@
 package com.samebug.sandbox.crashtest
 
 import com.typesafe.scalalogging.LazyLogging
+import io.sentry.Sentry
+import io.sentry.event.BreadcrumbBuilder
 import org.scalatest.{BeforeAndAfterAll, FunSpec}
+
+import scala.util.control.NonFatal
 
 class CrashReportTest extends FunSpec with BeforeAndAfterAll with LazyLogging {
   it("simply crashes") {
-    crasher.chainedException(List(3))
+    try {
+      Sentry.getContext.recordBreadcrumb(new BreadcrumbBuilder().setMessage("User made an action").build)
+      Sentry.capture("This is a test")
+      crasher.chainedException(List(3))
+    } catch {
+      case NonFatal(x) => Sentry.capture(x)
+    }
   }
 
   it("crashes with a cause chain") {
-    crasher.chainedException(List(3,5,7))
+    try {
+      crasher.chainedException(List(3, 5, 7))
+    } catch {
+      case NonFatal(x) => Sentry.capture(x)
+    }
   }
 
   private val crasher = new CrashReport
+  Sentry.init("https://1211268ba00f4d88bf6be37f4d0d1fbc:b9a94423a3054600b4e8599e44b5ebcb@sentry.io/259617")
 }
