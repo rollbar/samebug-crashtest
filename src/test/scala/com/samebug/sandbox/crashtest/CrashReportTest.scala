1d899b2e5fa0f7208b00bbbd68c64f1657963fdf
Poroszkai Daniel <daniel.poroszkai@samebug.io>
2017-12-15 11:45:34 +0100

    explicit catch

--- src/test/scala/com/samebug/sandbox/crashtest/CrashReportTest.scala
+++ src/test/scala/com/samebug/sandbox/crashtest/CrashReportTest.scala
@@ -3,13 +3,23 @@ package com.samebug.sandbox.crashtest
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{BeforeAndAfterAll, FunSpec}
 
+import scala.util.control.NonFatal
+
 class CrashReportTest extends FunSpec with BeforeAndAfterAll with LazyLogging {
   it("simply crashes") {
-    crasher.chainedException(List(3))
+    try {
+      crasher.chainedException(List(3))
+    } catch {
+      case NonFatal(x) => throw x
+    }
   }
 
   it("crashes with a cause chain") {
-    crasher.chainedException(List(3,5,7))
+    try {
+      crasher.chainedException(List(3,5,7))
+    } catch {
+      case NonFatal(x) => throw x
+    }
   }
 
   private val crasher = new CrashReport
