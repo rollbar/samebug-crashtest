8fc0ccaf3986ae7cd16ccbe1a9f92fa1cfd7538d
Poroszkai Daniel <daniel.poroszkai@samebug.io>
2017-12-15 10:45:24 +0100

    rollbar

--- src/test/scala/com/samebug/sandbox/crashtest/CrashReportTest.scala
+++ src/test/scala/com/samebug/sandbox/crashtest/CrashReportTest.scala
@@ -1,16 +1,36 @@
 package com.samebug.sandbox.crashtest
 
+import com.rollbar.notifier.Rollbar
+import com.rollbar.notifier.config.ConfigBuilder
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{BeforeAndAfterAll, FunSpec}
 
+import scala.util.control.NonFatal
+
 class CrashReportTest extends FunSpec with BeforeAndAfterAll with LazyLogging {
   it("simply crashes") {
-    crasher.chainedException(List(3))
+    try {
+      crasher.chainedException(List(3))
+    } catch {
+      case NonFatal(x) => rollbar.error(x)
+    }
   }
 
   it("crashes with a cause chain") {
-    crasher.chainedException(List(3,5,7))
+    try {
+      crasher.chainedException(List(3,5,7))
+    } catch {
+      case NonFatal(x) => rollbar.error(x)
+    }
   }
 
   private val crasher = new CrashReport
+  private val rollbar = {
+    val config = ConfigBuilder
+      .withAccessToken(System.getenv("ROLLBAR_ACCESSTOKEN"))
+      .environment("development")
+      .codeVersion("1.0.0")
+      .build
+    Rollbar.init(config)
+  }
 }
